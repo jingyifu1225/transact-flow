5f0ae0f2e95067fc1b12b0ec4f064704a8ed3fbb
Jingyi <jingyifu@ufl.edu>
2024-09-26 14:44:16 -0400

    Environmental Setup

---
+++ src/main/java/dev/jingyi/TransactFlow/TransactFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.jingyi.TransactFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactFlowApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/dev/jingyi/TransactFlow/TransactFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jingyi.TransactFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
